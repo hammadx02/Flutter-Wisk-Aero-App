a7c9826fd67a8912488849715c53aa450a29dea5
Hammad Ali <hammadaali02@gmail.com>
2024-08-15 22:46:36 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconly/iconly.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,6 +19,29 @@ class HomeScreen extends StatelessWidget {
         centerTitle: false,
         backgroundColor: Colors.transparent,
         elevation: 0,
+        actions: [
+          Container(
+            height: 50,
+            width: 50,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0XFFFFFFFF),
+            ),
+            child: const Icon(IconlyLight.notification),
+          ),
+          const SizedBox(
+            width: 7,
+          ),
+          const CircleAvatar(
+            radius: 25,
+            backgroundImage: NetworkImage(
+              'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+        ],
       ),
       backgroundColor: Colors.grey[300],
     );
