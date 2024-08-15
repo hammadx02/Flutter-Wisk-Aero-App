03b008ab61b4865fd97323384b35f4b0471db80a
Hammad Ali <hammadaali02@gmail.com>
2024-08-15 23:38:08 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:iconly/iconly.dart';
-import 'package:iconsax/iconsax.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
