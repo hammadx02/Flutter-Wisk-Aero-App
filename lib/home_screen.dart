54c5fc5cd7ef8f361e1831f8c16ef0c3d27f2fb3
Hammad Ali <hammadaali02@gmail.com>
2024-08-15 22:22:59 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Wisk',
+          style: GoogleFonts.notoSansTifinagh(),
+        ),
+        centerTitle: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      backgroundColor: Colors.grey[300],
+    );
   }
 }
